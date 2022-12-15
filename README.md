46c76a0115faf72a3cf87086c48d97e50100adde
Itzzzyashu <sakshamvirmani86@gmail.com>
2022-12-15 18:38:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Hey! I'm yash from India.
+Visit my website (click image)
+v6.2+
+<a href="https://itzzzyashu-cf.tk">
+<img src="https://telegra.ph/file/de13a51b41761a6e986da.jpg" />
+</a>
---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+});
+
+$(document).on("click", "ul li", function () {
+  $(this).addClass("n-active").siblings().removeClass("n-active");
+});
+
+$(document).onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
